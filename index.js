e371aed53b0ec653945bbe921c9652a0858a1db9
Harsh Mittal <mittalharsh4321@gmail.com>
2021-01-14 16:45:48 +0530

    env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const env = require("./config/environment");
 const app = express();
 require("./config/view-helper")(app);
-const port = 1000;
+const port = process.env.PORT;
 const logger = require("morgan");
 
 const path = require("path");
