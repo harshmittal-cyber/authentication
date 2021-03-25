c52631597b23290ed9385030ec32ec468da35348
Harsh Mittal <mittalharsh4321@gmail.com>
2021-03-25 14:27:40 +0530

    port chnaged

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const env = require("./config/environment");
 const app = express();
 require("./config/view-helper")(app);
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const logger = require("morgan");
 const bodyParser = require("body-parser");
 
